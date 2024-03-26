62d4206a1f6a7fa98278a2e04a6b30f88d3896fd
Ethan Hall <ethanwhall@icloud.com>
2024-03-26 19:58:23 -0400

    Remove round cursor, add new projects

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-const body = document.body;
+// const body = document.body;
 
-const cursor = document.querySelector('#cursor');
-const clickables = document.querySelectorAll('a');
+// const cursor = document.querySelector('#cursor');
+// const clickables = document.querySelectorAll('a');
 
-body.addEventListener("mousemove", (event) => {
-    cursor.style.transform = `translate(${event.clientX - 8}px, ${event.clientY - 8}px)`
-})
+// body.addEventListener("mousemove", (event) => {
+//     cursor.style.transform = `translate(${event.clientX - 8}px, ${event.clientY - 8}px)`
+// })
 
-body.addEventListener('mouseenter', (event) => {
-    cursor.style.display = 'block';
-})
+// body.addEventListener('mouseenter', (event) => {
+//     cursor.style.display = 'block';
+// })
 
-body.addEventListener('mouseleave', (event) => {
-    cursor.style.display = 'none';
-})
+// body.addEventListener('mouseleave', (event) => {
+//     cursor.style.display = 'none';
+// })
 
-clickables.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        cursor.style.width = '10px';
-        cursor.style.height = '10px';
-    })
-    
-    item.addEventListener('mouseout', () => {
-        cursor.style.width = '16px';
-        cursor.style.height = '16px';
-    })
-})
+// clickables.forEach(item => {
+//     item.addEventListener('mouseover', () => {
+//         cursor.style.width = '10px';
+//         cursor.style.height = '10px';
+//     })
 
+//     item.addEventListener('mouseout', () => {
+//         cursor.style.width = '16px';
+//         cursor.style.height = '16px';
+//     })
+// })
 
 // Hover profile photo
 
@@ -44,7 +43,6 @@ clickables.forEach(item => {
 //     profilePhoto.classList.add('opacity-0');
 //     profilePhoto.style.transform = 'translate(0px, 10px)';
 // })
-
 
 // Hover Skills
 // const skills = document.querySelectorAll('#skill-logo');
