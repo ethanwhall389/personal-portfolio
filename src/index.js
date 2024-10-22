27d27052f10b722194d591bd4f8a6542495c85e1
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2024-10-22 13:55:55 +0000

    Add form submission ability

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 // Form submission
-const form = document.querySelector('#contact-form');
-const messageOK = document.querySelector('#message-ok');
-const messageError = document.querySelector('#message-error');
+// const form = document.querySelector('#contact-form');
+// const messageOK = document.querySelector('#message-ok');
+// const messageError = document.querySelector('#message-error');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
 
-    const formData = new FormData(form);
+//     const formData = new FormData(form);
+//     const endpoint = 'https://forms.un-static.com/forms/5c34e40fc047c8fbeb5a41d0e12fa8a8249d1d33';
 
-    fetch('google.com', {
-        method: 'POST',
-        body: formData,
-    })
-    .then(response => {
-        if (response.ok) {
-            // If the submission was successful, show the success message
-            messageOK.style.display = 'block';
-        } else {
-             // If there was an error, show the error message
-             messageError.style.display = 'block';
-        }
-    })
-    .catch(() => {
-        // Handle any errors that occur during the fetch
-        messageError.style.display = 'block';
-    })
-})
+//     fetch(endpoint, {
+//         method: 'POST',
+//         body: formData,
+//         mode: 'no-cors',
+//     })
+//     .then(response => {
+//         if (response.ok) {
+//             // If the submission was successful, show the success message
+//             messageOK.style.display = 'block';
+//         } else {
+//              // If there was an error, show the error message
+//              messageError.style.display = 'block';
+//         }
+//     })
+//     .catch(() => {
+//         // Handle any errors that occur during the fetch
+//         messageError.style.display = 'block';
+//     })
+// })
